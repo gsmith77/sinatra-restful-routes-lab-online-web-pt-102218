2da010bc9290c739806a5f26b34e9d2d0bab493f
grant smith <grantsmith1256@gmail.com>
2019-02-18 20:43:55 +0000

    Automatically backed up by Learn

--- db/migrate/20190218204252_create_recipes.rb
+++ db/migrate/20190218204252_create_recipes.rb
@@ -1,4 +1,8 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.timestamp :cook_time
   end
 end
